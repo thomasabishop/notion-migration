b1a1e4c78c5f141299b8c0582cbc72f5de298336
thomasabishop <tactonbishop@gmail.com>
2022-01-30 10:25:29 +0000

    Remove  standalone create dir func

--- src/services/createDirectory.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-module.exports = function (directoryPath) {
-  return !fs.existsSync(directoryPath)
-    ? fs.mkdirSync(directoryPath)
-    : console.warn(`Directory /${directoryPath} already exists`);
-};
