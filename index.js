1d1749894a68af1adb89bd9410d37b7af4db1b75
thomasabishop <tactonbishop@gmail.com>
2022-01-30 10:24:50 +0000

    Restructure init function

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const { Client } = require('@notionhq/client')
 
 const notionApiKey = require('./src/api/auth/notionApiKey')
 const databaseUniqueId = require('./src/api/auth/databaseId')
+const { createDir, deleteDir } = require('./src/services/io')
+const globals = require('./src/globals')
 const getPageIds = require('./src/api/getPageIds')
 const getPageContents = require('./src/api/getPageContents')
 const parsePageBlocks = require('./src/services/parsePageBlocks')
 
 async function migrate() {
-  const mainDirectory = 'migrated_project'
+  deleteDir(`${globals.MAIN_DIR}`)
+  createDir(`${globals.MAIN_DIR}`)
+  createDir(`${globals.MAIN_DIR}/img`)
   const notion = new Client({ auth: notionApiKey })
   let pageIds = await getPageIds(databaseUniqueId, notion)
   let firstPage = await getPageContents(pageIds[0], notion)
